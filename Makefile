02c337a9910ec5aaacbb0c05f0b06488465a3039
nulltask <nulltask@gmail.com>
2012-11-02 23:16:09 +0900

    add test

---
+++ Makefile
@@ -0,0 +1,15 @@
+
+build:
+	@browserbuild -d -g math -m math -b lib/ lib > dist/math.js
+
+test:
+	@mocha
+
+test-browser: build
+	@serve . &
+	@open http://localhost:3000/test
+	
+test-phantomjs: build
+	@mocha-phantomjs test/index.html
+
+.PHONY: build test test-browser test-phantomjs
---
+++ test/test.js
@@ -0,0 +1,50 @@
+
+/**
+ * Bi-side test.
+ */
+
+describe('Math', function() {
+  describe('.PI', function() {
+    it('円周率 3.14', function() {
+      Math.PI.should.be.equal(3.141592653589793);
+    });
+  });
+
+  describe('.abs()', function() {
+    it('正の整数: 5', function() {
+      Math.abs(5).should.be.equal(5);
+    });
+    it('負の整数: -5', function() {
+      Math.abs(-5).should.be.equal(5);
+    });
+    it('正の小数:  5.5', function() {
+      Math.abs(5.5).should.be.equal(5.5);
+    });
+    it('負の小数: -5.5', function() {
+      Math.abs(-5.5).should.be.equal(5.5);
+    });
+  });
+});
+
+/**
+ * Browser specific test.
+ */
+
+if (typeof exports !== 'undefined') return;
+
+describe('jQuery', function() {
+  it('$', function() {
+    $.should.be.a('function');
+  });
+  
+  it('#test p:first', function() {
+    $('#test').find('p').first().text().should.be.equal('This is test.');
+  });
+  
+  it('#test span:first (ajax)', function() {
+    $.get('./ajax.txt', function(data) {
+      var $target = $('#test').find('span').first().text(data);
+      $target.text().should.be.equal('This is ajax content.');
+    });
+  });
+});
